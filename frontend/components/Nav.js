c1c822236b632bcdc91504c1d1b1743173940f9d
Chervona <nastya02chervona@gmail.com>
2019-06-03 22:51:59 +0300

    test: testing_2

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -9,7 +9,7 @@ import Signout from './Signout'
 const Nav = () => (
   <User>
     {({ data: { me } }) => (
-      <NavStyles>
+      <NavStyles data-test='nav'>
         <Link href='/shop'>
           <a>Shop</a>
         </Link>
